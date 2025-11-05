69090c3419fc344f096ea6df9eaed2124853bba3
Luis R Zunniga S <luiscr1104@gmail.com>
2025-11-05 14:02:31 -0600

    Agregi galery

---
+++ src/components/Galery.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { ParallaxScrollSecond } from "./ui/parallax-scroll-2";
+
+export function ParallaxScrollSecondDemo() {
+  return <ParallaxScrollSecond images={images} />;
+}
+
+const images = [
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/c86779d7-53ff-44a0-0241-3804e2bba900/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/effbb4f2-933e-46ef-4d80-07f34f3c0000/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/70877784-f7c9-4b0a-fb28-ecdf3cbae900/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/0a7447bb-1b5a-40c1-ebd3-f834f030d500/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/f63082b1-eee4-4d02-7af3-455cdc8faf00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/896b7f54-7551-4270-6973-90742d2edd00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/b7fb96c9-e60e-48bc-9e0e-4f7c96b6c000/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/e1dfba7d-7dda-48ef-eb81-e6e0d5d9fc00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/ae1c2c8a-44db-4503-32e7-3d3bea799f00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/34ab21d4-0ce3-473f-324c-91c12ec0b100/public",  
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/c86779d7-53ff-44a0-0241-3804e2bba900/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/effbb4f2-933e-46ef-4d80-07f34f3c0000/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/70877784-f7c9-4b0a-fb28-ecdf3cbae900/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/0a7447bb-1b5a-40c1-ebd3-f834f030d500/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/f63082b1-eee4-4d02-7af3-455cdc8faf00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/896b7f54-7551-4270-6973-90742d2edd00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/b7fb96c9-e60e-48bc-9e0e-4f7c96b6c000/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/e1dfba7d-7dda-48ef-eb81-e6e0d5d9fc00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/ae1c2c8a-44db-4503-32e7-3d3bea799f00/public",
+  "https://imagedelivery.net/Lh5ivcu1Gl9SRBAdYRSP2g/34ab21d4-0ce3-473f-324c-91c12ec0b100/public",
+];
